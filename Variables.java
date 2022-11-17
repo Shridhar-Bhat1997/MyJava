918af0dbb4d1b4421b5a2ec8e330a80f357a0e92
Shridhar Bhat <bhatshridhar27@gmail.com>
2022-11-17 15:44:50 +0530

    Variables 17-Nov-2022

---
+++ Variables.java
@@ -0,0 +1,25 @@
+public class Variables
+{
+	public static void main(String args[])
+	{
+		long l=50000000; // 8 bytes
+		//System.out.print(l);
+		
+		int i=100;  // 4 bytes
+		//System.out.print(i);
+		
+		short s=5; // 2 bytes
+		//System.out.print(s);
+		
+		byte b=10; // 1 byte
+		//System.out.print(b);
+		
+		double percent=4.6;
+		//System.out.print(percent);
+		
+		char c='A';
+		c=66;
+		System.out.print(c);
+		
+	}
+}
